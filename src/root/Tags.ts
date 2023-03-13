4bb0c0867b9a0292bacb268fce2fce16177efe06
asapovk <asapovk@gmail.com>
2023-03-13 23:34:48 +0300

    add tags

--- src/root/Tags.ts
+++ src/root/Tags.ts
@@ -3,6 +3,79 @@ import { ITagAttributes, Element, Tree } from './NTree';
 export class Tags {
   constructor(private tree: Tree) {}
 
+  public text(
+    key: string,
+    opts: {
+      text: string;
+      className?: string;
+      style?: string;
+      getNode?: (e: HTMLElement) => void;
+      onClick?: (e) => void;
+    }
+  ) {
+    return this.tree.tag({
+      tagName: 'p',
+      child: opts.text,
+      key: key,
+      getNode: opts.getNode,
+      attributes: {},
+      eventListeners: opts.onClick
+        ? {
+            click: opts.onClick,
+          }
+        : undefined,
+    });
+  }
+
+  public headerText(
+    key: string,
+    opts: {
+      text: string;
+      size?: number;
+      className?: string;
+      style?: string;
+      getNode?: (e: HTMLElement) => void;
+      onClick?: (e) => void;
+    }
+  ) {
+    return this.tree.tag({
+      tagName: `h${opts.size || 1}`,
+      child: opts.text,
+      key: key,
+      getNode: opts.getNode,
+      attributes: {},
+      eventListeners: opts.onClick
+        ? {
+            click: opts.onClick,
+          }
+        : undefined,
+    });
+  }
+
+  public div(
+    key: string,
+    opts: {
+      child?: Element | string;
+      className?: string;
+      style?: string;
+      getNode?: (e: HTMLElement) => void;
+      onClick?: (e) => void;
+    }
+  ) {
+    return this.tree.tag({
+      tagName: 'div',
+      child: opts.child || '',
+      key: key,
+      getNode: opts.getNode,
+      attributes: {},
+      eventListeners: opts.onClick
+        ? {
+            click: opts.onClick,
+          }
+        : undefined,
+    });
+  }
+
   public button(
     key: string,
     opts: {
