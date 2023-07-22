f0cf17d58ba2f4ba27612b3b73a2fd9c7de3ac99
asapovk <asapovk@gmail.com>
2023-07-22 15:15:22 +0300

    more icons

--- playground/mailbox/src/App.ts
+++ playground/mailbox/src/App.ts
@@ -1,11 +1,21 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Tree, Root } from '../../../packages/core/lib/NTree';
 import { Sidebar } from './folders/components';
 import { LettersList } from './letters/components/LettersList';
 import { Notification } from './notification/components/Notification';
 import { Popup } from './popup/components/Window';
+import { MainMenu } from './main-menu/components';
+
 
 const tree = new Tree({
-  makeElement: (tag) => document.createElement(tag),
+  //@ts-ignore
+  makeElement: (tag) => {
+    if (tag === 'svg' || tag === 'path') {
+      return document.createElementNS('http://www.w3.org/2000/svg', tag);
+    } else {
+      return document.createElement(tag);
+    }
+  },
 });
 
 export const Application = () =>
@@ -15,7 +25,7 @@ export const Application = () =>
       attributes: {
         id: 'app_root',
       },
-      child: [Sidebar(), Notification(), LettersList(), Popup()],
+      child: [Sidebar(), Notification(), LettersList(), Popup(), MainMenu()],
     },
     'app_root'
   );
--- playground/mailbox/src/main-menu/components/index.ts
+++ playground/mailbox/src/main-menu/components/index.ts
@@ -1,12 +1,22 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Tree } from '../../../../../packages/core/lib/NTree';
 import { Tags } from '../../../../../packages/core/lib/Tags';
+import { Search } from '../../../../__shared/ui/Svg/Search';
+import { Settings } from '../../../../__shared/ui/Svg/Settings';
 import { IState } from '../../_redux/types';
 import { Reflexio } from '../../root-redux/reflector';
-
+import './styles.less';
 //const vertialMenuItems  =  ['profile', 'contacts', 'setting' ]
 
 const tree = new Tree({
-  makeElement: (tag: string) => document.createElement(tag),
+  //@ts-ignore
+  makeElement: (tag) => {
+    if (tag === 'svg' || tag === 'path') {
+      return document.createElementNS('http://www.w3.org/2000/svg', tag);
+    } else {
+      return document.createElement(tag);
+    }
+  },
 });
 const tags = new Tags(tree);
 const reflexio = new Reflexio();
@@ -23,8 +33,52 @@ export const MainMenu = () => {
     },
     child: [
       tags.div({
+        className: 'main-button',
+        child: tags.div(
+          {
+            className: 'horizontal-menu-item',
+            child: Search(tags, {}, 'main-main-icon'),
+          },
+          'main-main-div'
+        ),
+      }),
+      tags.div({
+        className: 'horizontal-menu',
+        child: [
+          tags.div(
+            {
+              className: 'menu-item',
+              child: Settings(tags, {}, 'main-hor-search-1'),
+            },
+            'main-hor-search-1-r'
+          ),
+          tags.div(
+            {
+              className: 'menu-item',
+              child: Settings(tags, {}, 'main-hor-search-2'),
+            },
+            'main-hor-search-2-r'
+          ),
+          tags.div(
+            {
+              className: 'menu-item',
+              child: Settings(tags, {}, 'main-hor-search-3'),
+            },
+            'main-hor-search-3-r'
+          ),
+        ],
+      }),
+      tags.div({
         className: 'vertical-menu',
-        child: null,
+        child: [
+          tags.div(
+            {
+              className: 'menu-item',
+              child: Search(tags, {}, 'main-ver-icon'),
+            },
+            'main-ver-div'
+          ),
+        ],
       }),
     ],
   });
