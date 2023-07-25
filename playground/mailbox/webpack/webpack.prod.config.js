0e7c4eba0f0a8410e316553226433d8b26e45d16
asapovk <asapovk@gmail.com>
2023-07-25 03:15:45 +0300

    build

---
+++ playground/mailbox/webpack/webpack.prod.config.js
@@ -0,0 +1,15 @@
+const baseConfig = require('./webpack.base.config');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = Object.assign(baseConfig, {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    ...baseConfig.plugins,
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[hash].css',
+    }),
+  ],
+});
